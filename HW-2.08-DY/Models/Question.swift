9145f2e7c3f3799ac5f2b5c5358df7fca5c866b7
Denis13s <141165300+Denis13s@users.noreply.github.com>
2023-10-15 16:47:50 +0200

    initial commit

---
+++ HW-2.08-DY/Models/Question.swift
@@ -0,0 +1,8 @@
+//
+//  Question.swift
+//  HW-2.08-DY
+//
+//  Created by Denis Yarets on 15/10/2023.
+//
+
+import Foundation
---
+++ HW-2.08-DY/ViewControllers/ResultViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ResultViewController.swift
+//  HW-2.08-DY
+//
+//  Created by Denis Yarets on 15/10/2023.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
